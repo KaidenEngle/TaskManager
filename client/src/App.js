f7c15e7b2bb91a50e9827ec0794244c0ac0d92e5
Kai'den Engle <146896923+KaidenEngle@users.noreply.github.com>
2024-12-31 23:10:55 -0500

    almost done

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,55 @@
-import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 
-//data will be string sent from server
-const apiCall = () => {
-  axios.get("http://localhost:3000").then((data) => {
-    //logs data in front end console
-    console.log(data);
-  });
-};
+function TaskInput( {value, onChange} ){
+  return (
+    <input type="text" placeholder="Enter task" value={value} onChange={onChange} className="Task-input"/>
+  )
+}
+
+function TaskButton( {onClick} ){
+  return (
+    <button onClick={onClick} className="Task-button">Add Task</button>
+  )
+}
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleInputChange = (event) => {
+    setTask(event.target.value);
+  }
+
+  const handleButtonClick = () => {
+    if (task.trim()) {
+        setTasks([...tasks, task]);
+        setTask("");
+    }
+  }
+
+  const HandleDeleteTaskClick = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={apiCall}>Call API</button>
+        <h1 className="App-title">Locked In Task Manager</h1>
+        <div className="Task-input-container">
+          <TaskInput value={task} onChange={handleInputChange}/>
+        </div>
+        <div className="Task-button-container">
+          <TaskButton onClick={handleButtonClick}/>
+        </div>
+        <ul className={`Task-list ${tasks.length > 0 ? "Task-list-bordered" : ""}`}>
+          {tasks.map((task, index) => (
+              <li key={index}>{
+                task}
+                <button onClick={()=> HandleDeleteTaskClick(index)}>Delete Task</button>
+              </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
